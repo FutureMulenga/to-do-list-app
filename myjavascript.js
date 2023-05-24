ec3b88aecbba39920defe1295dae186cd8eaa91c
FutureMulenga <85027965+FutureMulenga@users.noreply.github.com>
2023-05-25 01:46:59 +0200

    Add files via upload
    version 1.0

---
+++ myjavascript.js
@@ -0,0 +1,32 @@
+
+var inputBox = document.getElementById("input-box");
+var listContainer = document.getElementById("list-container");
+
+
+//function to add task
+function addTask(){
+
+    if(inputBox.value == ''){
+        alert("You must write something!");
+    }
+    else{
+
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+
+        let span = document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+}
+
+listContainer.addEventListener("click", function(e){
+    if (e.target.tagName ==="LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName ==="SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
